5cb9085722c34f55bbddfd79bfd420b78d1d7319
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-04-26 17:47:41 +0530

    Able to delete and edit items in the list

--- app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
@@ -1,6 +1,7 @@
 package com.example.myshoppinglistapp
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,9 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -66,9 +69,28 @@ fun ShoppingListApp() {
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            items(sItems) {
-                ShoppingListItem(item = it, onEditClick = { /*TODO*/ }) {
-
+            items(sItems) { item ->
+                if (item.isEditing) {
+                    ShoppingItemEditor(item = item, onEditComplete = { editedName, editedQuantity ->
+                        sItems = sItems.map {
+                            it.copy(isEditing = false)
+                        }
+                        val editedItem = sItems.find {
+                            it.id == item.id
+                        }
+                        editedItem?.let {
+                            it.name = editedName
+                            it.quantity = editedQuantity
+                        }
+                    })
+                } else {
+                    ShoppingListItem(item = item, onEditClick = {
+                        sItems = sItems.map {
+                            it.copy(isEditing = it.id == item.id)
+                        }
+                    }, onDeleteClick = {
+                        sItems = sItems - item
+                    })
                 }
             }
         }
@@ -129,6 +151,54 @@ fun ShoppingListApp() {
 }
 
 @Composable
+fun ShoppingItemEditor(item: ShoppingItem, onEditComplete: (String, Int) -> Unit) {
+    var editedName by remember {
+        mutableStateOf(item.name)
+    }
+    var editedQuantity by remember {
+        mutableStateOf(item.quantity.toString())
+    }
+    var isEditing by remember {
+        mutableStateOf(item.isEditing)
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Column {
+            BasicTextField(
+                value = editedName,
+                onValueChange = { editedName = it },
+                singleLine = true,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(8.dp)
+            )
+            BasicTextField(
+                value = editedQuantity,
+                onValueChange = { editedQuantity = it },
+                singleLine = true,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(8.dp)
+            )
+        }
+
+        Button(onClick = {
+            isEditing = false
+            onEditComplete(editedName, editedQuantity.toIntOrNull() ?: 1)
+        }) {
+            Text(text = "Save")
+        }
+    }
+}
+
+
+@Composable
 fun ShoppingListItem(
     item: ShoppingItem,
     onEditClick: () -> Unit,
@@ -141,15 +211,16 @@ fun ShoppingListItem(
             .border(
                 border = BorderStroke(2.dp, Color(0XFF018786)),
                 shape = RoundedCornerShape(20)
-            )
+            ),
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(text = item.name, modifier = Modifier.padding(8.dp))
         Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
         Row(modifier = Modifier.padding(8.dp)) {
-            IconButton(onClick = { onEditClick }) {
+            IconButton(onClick = { onEditClick() }) {
                 Icon(imageVector = Icons.Default.Edit, contentDescription = null)
             }
-            IconButton(onClick = { onDeleteClick }) {
+            IconButton(onClick = { onDeleteClick() }) {
                 Icon(imageVector = Icons.Default.Delete, contentDescription = null)
             }
         }
